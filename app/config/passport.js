bd7a913cbeb583e1f92c6ff443e65eea42b748fd
Abdul Rehman <mehars.6925@gmail.com>
2018-11-17 16:28:06 +0500

    made auth work

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,5 +1,5 @@
 const JWTStrategy = require('passport-jwt').Strategy;
-const ExtractJWT = require('passport-jwt').ExtractJwt;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const secret = require('./secret');
 const User = require('../models/user');
@@ -8,10 +8,10 @@ const User = require('../models/user');
 module.exports = (passport) => {
     let opts = {};
 
-    opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('JWT');
     opts.secretOrKey = secret.appSecret;
 
-    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    passport.use('jwt',new JWTStrategy(opts, (jwt_payload, done) => {        
         User.findOne({ id: jwt_payload.sub }).exec()
             .then(user => {
                 if(user) {
