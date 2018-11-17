d784f5fea923e9afb6d5019b07bb5f659f9eaa7c
Abdul Rehman <mehars.6925@gmail.com>
2018-11-17 16:26:20 +0500

    configured passportjs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 const express = require('express');
+const passport = require('passport');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
@@ -9,11 +10,18 @@ const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/sosharu', { useNewUrlParser: true });
 
 const usersController = require('./app/controllers/users');
+const passportConfing = require('./app/config/passport');
+
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passportConfing(passport);
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersController);
