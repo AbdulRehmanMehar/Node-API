a4a4ef61e56543287f56c6efc4f51a1da3f00089
Abdul Rehman <mehars.6925@gmail.com>
2018-11-17 16:28:38 +0500

    protected some routes and added validation, verification

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 const User = require('../models/user');
+const secret = require('../config/secret');
+const mailer = require('../config/nodemailer');
 
-router.get('/', (req, res) => {
-    User.find({}).select('-__v').exec()
+
+
+
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    User.find({}).select('-__v -password -socialConnection -email').exec()
         .then(users => {
             res.status(200).json({
                 success: true,
@@ -24,23 +31,66 @@ router.get('/', (req, res) => {
 });
 
 
-router.post('/register', (req, res) => {
-    let user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 10),
-        photo: {
-            profile: (req.body.profile != null) ? req.body.profile : 'default-profile.png',
-            cover: (req.body.cover != null) ? req.body.cover : 'default-cover.png'
-        }
-    });
-    user.save()
-        .then(result => {
-            res.status(201).json({
-                success: true,
-                message: 'User is Registered',
-                data: result
+router.get('/check/uname/:uname', (req, res) => {
+    User.findOne({username: req.body.username}).select('-password -__v').exec()
+        .then(user => {
+            if(user) {
+                res.status(200).json({
+                    success: true,
+                    message: 'User exist with this email',
+                    data: user
+                });
+            }else{
+                res.status(400).json({
+                    success: false,
+                    message: 'User does\'nt Exist',
+                    data: false,
+                });
+            }
+        }).catch(error => {
+            res.status(500).json({
+                success: false,
+                message: 'Something went wrong',
+                data: error
             });
+        });
+});
+
+router.post('/register', (req, res) => {
+    User.findOne({email: req.body.email}).exec()
+        .then(record => {
+            if(record){
+                res.status(400).json({
+                    success: false,
+                    message: "User already exists with this email"
+                });
+            }else{
+                let user = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    username: req.body.username,
+                    password: bcrypt.hashSync(req.body.password, 10),
+                    photo: {
+                        profile: (req.body.profile != null) ? req.body.profile : 'default-profile.png',
+                        cover: (req.body.cover != null) ? req.body.cover : 'default-cover.png'
+                    },
+                });
+                user.save()
+                    .then(result => {
+                        mailer.registration(result); // send mail
+                        res.status(201).json({
+                            success: true,
+                            message: 'User is Registered with confirmation link to email',
+                            data: result
+                        });
+                    }).catch(error => {
+                        res.status(500).json({
+                            success: false,
+                            message: 'Something went wrong',
+                            data: error
+                        });
+                    });
+            }
         }).catch(error => {
             res.status(500).json({
                 success: false,
@@ -49,10 +99,57 @@ router.post('/register', (req, res) => {
             });
         });
     
+    
+});
+
+router.post('/login', (req, res) => { // Email can be username or email
+    User.findOne({$or: [{email: req.body.email}, {username: req.body.email}]})
+        .select('-__v')
+        .exec()
+        .then(user => {
+            if(user) {
+                bcrypt.compare(req.body.password, user.password, (error, success) => {
+                    if(success) {
+                        const token = jwt.sign({_id: user._id}, secret.appSecret, {expiresIn: 604800});
+                        user.password = null;
+                        res.status(200).json({
+                            success: true,
+                            message: "Token Created",
+                            data: {
+                                token: "JWT " +token,
+                                user: user
+                            }
+                        });
+                    }
+                    if(error) {
+                        res.status(400).json({
+                            success: false,
+                            message: "User Credentials don't match",
+                            data: error
+                        });
+                    }
+                    console.log(error, success)
+                });
+                // res.status(200).json({success: true, message: "Exists", data: user})
+            }
+            else{
+                res.status(400).json({
+                    success: false,
+                    message: "User not found",
+                    data: null
+                });
+            }
+        }).catch(error => {
+            res.status(500).json({
+                success: false,
+                message: "Something went wrong => find",
+                data: error
+            });
+        });
 });
 
 // Get User By ID; ALWAYS USE DYNAMIC URLS BELOW THE STATIC URLS SO IT MAY ONLY GET CALLED IF NO STATIC URL IS FOUND
-router.get('/:uid', (req, res) => {
+router.get('/:uid', passport.authenticate('jwt', {session: false}), (req, res) => {
     const uid = req.params.uid;
     User.findById(uid).select('-password -__v').exec()
         .then(user => {
@@ -78,7 +175,7 @@ router.get('/:uid', (req, res) => {
         });
 });
 
-router.patch('/:uid', (req, res) => {
+router.patch('/:uid', passport.authenticate('jwt', {session: false}), (req, res) => {
     const uid = req.params.uid;
     User.findById(uid).select('-posts -friends -__v').exec()
         .then(user => {
@@ -149,7 +246,7 @@ router.patch('/:uid', (req, res) => {
         });
 });
 
-router.delete('/:uid', (req, res) => {
+router.delete('/:uid', passport.authenticate('jwt', {session: false}), (req, res) => {
     const uid = req.params.uid;
     User.findById(uid).exec()
         .then(user => {
